ee1c39839264fae9bf7242a57a35faf220978c61
Lezrec <apbcrim2@gmail.com>
2019-03-03 18:31:37 -0700

    Update tests

--- MatrixTester/Program.cs
+++ MatrixTester/Program.cs
@@ -11,7 +11,14 @@ namespace MatrixTester {
 
         public static void Main(string[] args) {
             IntegerMatrix m1 = new IntegerMatrix(2, 3);
+            m1.Replace(1, 1, 5);
             Console.WriteLine(m1.ToString());
+            IntegerMatrix m2 = new IntegerMatrix(2, 3);
+            m2.Replace(0, 2, 15);
+            IntegerMatrix m3 = (IntegerMatrix)m1.AppendByRow(m2);
+            IntegerMatrix m4 = (IntegerMatrix)m1.AppendByColumn(m2);
+            Console.WriteLine(m3.ToString());
+            Console.WriteLine(m4.ToString());
             Console.Read();
         }
 
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -7,14 +7,108 @@ namespace UnitTests {
 
     [TestClass]
     public class UnitTester {
-        
+
+        private Random rand = new Random(); //TODO: Figure out whether to keep the same seed every time or not.
+
+        public IntegerMatrix GenerateRandomIntegerMatrix() {
+            int row = rand.Next(1, 10);
+            int col = rand.Next(1, 10);
+            IntegerMatrix ret = new IntegerMatrix(row, col);
+
+            for(int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    ret.Replace(i, j, rand.Next(9, 255));
+                }
+            }
+            return ret;
+        }
+
         [TestMethod]
-        public void Matrix_Construction_Valid_Test() {
+        public void Integer_Matrix_Construction_Size_Test() {
             //Arrange
-            
+            int row = rand.Next(1, 10);
+            int col = rand.Next(1, 10);
+            IntegerMatrix m1 = new IntegerMatrix(row, col);
             //Act
 
             //Assert
+            Assert.AreEqual(m1.RowSize, row);
+            Assert.AreEqual(m1.ColumnSize, col);
+        }
+
+        [TestMethod]
+        public void Integer_Matrix_Value_Change_Test() {
+            //Arrange
+            int row = rand.Next(1, 10);
+            int col = rand.Next(1, 10);
+            int rowIndex = rand.Next(1, row - 1);
+            int colIndex = rand.Next(1, col - 1);
+            IntegerMatrix m1 = new IntegerMatrix(row, col);
+            //Act
+            m1.Replace(rowIndex, colIndex, 5); //value doesnt matter just cant be zero
+            //Assert
+            Assert.AreEqual(m1.Get(rowIndex, colIndex), 5);
+        }
+
+        [TestMethod]
+        public void Matrix_Transpose_Test() {
+            //Arrange
+            IntegerMatrix m1 = GenerateRandomIntegerMatrix();
+            IntegerMatrix m2 = (IntegerMatrix)m1.GetTranspose();
+
+            for (int i = 0; i < m1.RowSize; i++) {
+                for (int j = 0; j < m1.ColumnSize; j++) {
+                    Assert.AreEqual(m1.Get(i, j), m2.Get(j, i));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Row_Append_Test() {
+            //Arrange
+            IntegerMatrix m1 = GenerateRandomIntegerMatrix();
+            int col = rand.Next(1, 10);
+            IntegerMatrix m2 = new IntegerMatrix(m1.RowSize, col);
+            //Act
+            IntegerMatrix m3 = (IntegerMatrix)m1.AppendByRow(m2);
+            //Assert
+            for(int i = 0; i < m1.RowSize; i++) {
+                for (int j = m1.ColumnSize; j < m3.ColumnSize; j++) {
+                    Assert.AreEqual(m3.Get(i, j), 0);
+                }
+            }
+           
+        }
+
+        [TestMethod]
+        public void Column_Append_Test() {
+            //Arrange
+            IntegerMatrix m1 = GenerateRandomIntegerMatrix();
+            int row = rand.Next(1, 10);
+            IntegerMatrix m2 = new IntegerMatrix(row, m1.ColumnSize);
+            //Act
+            IntegerMatrix m3 = (IntegerMatrix)m1.AppendByColumn(m2);
+            //Assert
+            for (int i = m1.RowSize; i < m3.RowSize; i++) {
+                for (int j = 0; j < m1.ColumnSize; j++) {
+                    Assert.AreEqual(m3.Get(i, j), 0);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Row_Remove_Test() {
+
+        }
+
+        [TestMethod]
+        public void Column_Remove_Test() {
+
+        }
+
+        [TestMethod]
+        public void Generate_Test() {
+
         }
     }
 }
